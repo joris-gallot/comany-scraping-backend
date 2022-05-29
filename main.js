8643a17f701d574416e29a4b14a2ae3e009cdde4
Joris Gallot <70059224+joris-gallot@users.noreply.github.com>
2022-05-29 14:13:09 +0200

    chore: heroku auto deploy

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import fastifyCors from "@fastify/cors";
+import dotenv from "dotenv";
 import _fastify from "fastify";
 import CompanyService from "./src/services/CompanyService.js";
+
+dotenv.config();
 
 const fastify = _fastify({ logger: true });
 fastify.register(fastifyCors);
@@ -19,7 +22,7 @@ fastify.get("/search", (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen(3001);
+    await fastify.listen(process.env.PORT || 3001, "0.0.0.0");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
